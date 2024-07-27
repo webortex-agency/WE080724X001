0e4246cd805cb16a2c1644397ea3dd81e2705dc5
webortex <webortex@gmail.com>
2024-07-27 17:37:17 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,7 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      boxShadow: {
-        colorShadow:
-          "rgba(143, 22, 207, 0.5) 0px 0px 30px 5px, rgba(96, 107, 255, 0.5) 0px 0px 15px 5px, rgba(96, 107, 255, 0.5) 0px 0px 15px 15px !important",
-      },
+
     },
   },
   plugins: [],
