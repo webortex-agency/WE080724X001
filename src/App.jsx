a194342497942f69227e6658008601396d67add1
JAYADEEPREDDYPEDDIREDDY <138111452+JAYADEEPREDDYPEDDIREDDY@users.noreply.github.com>
2024-08-05 23:30:43 +0530

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import Button from "@mui/material/Button";
 import "@fontsource/poppins";
 import Home from "./Components/Home/Home";
+import Process from "./Components/Process/Process";
 
 const App = () => {
   return (
     <>
-      <Home />
+      <Process />
     </>
   );
 };
