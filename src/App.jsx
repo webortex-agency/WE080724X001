3edfdbe7a3351443b6ff0cb41576d0e7332828c4
sisindri <sisindrisingamsetti@gmail.com>
2024-08-07 21:38:49 +0530

    landing page update with teams section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "@fontsource/poppins";
 import Navbar from "./Components/Navabar/Navbar";
 import Home from "./Components/Home/Home";
-import Team from "./Components/Team/Team"
+import Team from "./Components/Team/Team";
 import Footer from "./Components/Footer/Footer";
 import Process from "./Components/Process/Process";
 
@@ -12,7 +12,7 @@ const App = () => {
       <Navbar />
       <Home />
       <Process />
-      <Team/>
+      <Team />
       <Footer />
     </>
   );
