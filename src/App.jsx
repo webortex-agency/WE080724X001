22be099c91741dad160f773d10c099616f53c4ac
ashokq <ashokpemeram@gmail.com>
2024-08-07 13:17:13 +0530

    Home Page Team Members Details Desktop View Completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Button from "@mui/material/Button";
 import "@fontsource/poppins";
 import Home from "./Components/Home/Home";
+import Team from "./Components/Team/Team"
 
 const App = () => {
   return (
     <>
       <Home />
+      <Team/>
     </>
   );
 };
