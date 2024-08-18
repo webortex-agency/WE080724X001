7de8e3d8658caae6e62f2d16880e9c1772d97ff5
sisindri <sisindrisingamsetti@gmail.com>
2024-08-18 22:53:36 +0530

    contact us page checking and working successfully

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,9 +1,9 @@
 import { Container, Button, Grid, TextField, Typography } from "@mui/material";
-import HandshakeIcon from '@mui/icons-material/Handshake';
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
-import emailjs from '@emailjs/browser';
-import { useRef } from 'react';
+import HandshakeIcon from "@mui/icons-material/Handshake";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import LocalPhoneOutlinedIcon from "@mui/icons-material/LocalPhoneOutlined";
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
 
 const Contact = () => {
   const form = useRef();
@@ -12,28 +12,41 @@ const Contact = () => {
     e.preventDefault();
 
     emailjs
-      .sendForm('service_p1dx12q', 'template_n2rio1e', form.current, 'Bc-7UM34x4W5ZfIp3')
+      .sendForm(
+        "service_p1dx12q",
+        "template_n2rio1e",
+        form.current,
+        "Bc-7UM34x4W5ZfIp3"
+      )
       .then(
         (result) => {
-          console.log('SUCCESS!', result.text);
-          alert('Message Sent Successfully!');
+          console.log("SUCCESS!", result.text);
+          alert("Message Sent Successfully!");
         },
         (error) => {
-          console.log('FAILED...', error.text);
-          alert('Failed to send the message, please try again.');
-        },
+          console.log("FAILED...", error.text);
+          alert("Failed to send the message, please try again.");
+        }
       );
   };
 
   return (
-
     <Container>
-      <Grid container sx={{ display: 'flex', justifyContent: "center", marginBottom: 10 }}>
-        <Grid item sx={{ display: 'flex', alignItems: "center", flexDirection: 'column', marginTop: 10 }}>
+      <Grid
+        container
+        sx={{ display: "flex", justifyContent: "center", marginBottom: 10 }}
+      >
+        <Grid
+          item
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+            marginTop: 10,
+          }}
+        >
           <div className="gap-4 md:inline-flex md:justify-start text-center md:text-left">
-            <HandshakeIcon
-              className="bg-[#CA87FF] text-[#722AAB] h-[70px] w-[70px] p-[14px] rounded-full"
-            />
+            <HandshakeIcon className="bg-[#CA87FF] text-[#722AAB] h-[70px] w-[70px] p-[14px] rounded-full" />
           </div>
           <br />
           <Typography
@@ -43,65 +56,183 @@ const Contact = () => {
             <span className="text-[#5956E8] mx-1">&#47;&#47;</span>
             01 . Contact us
           </Typography>
-          <Typography sx={{ textAlign: "center", color: "#DEDEDE", fontSize: "30px", fontFamily: "poppins" }}>
+          <Typography
+            sx={{
+              textAlign: "center",
+              color: "#DEDEDE",
+              fontSize: "30px",
+              fontFamily: "poppins",
+            }}
+          >
             Let’s build something awesome together
           </Typography>
         </Grid>
 
-        <Grid container sx={{ display: 'flex', justifyContent: "center", gap: { xs: 10, sm: 10, md: 20, lg: 20, xl: 20 }, flexDirection: 'row', marginTop: 10 }}>
-          <Grid xs={10}
+        <Grid
+          container
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            gap: { xs: 10, sm: 10, md: 20, lg: 20, xl: 20 },
+            flexDirection: "row",
+            marginTop: 10,
+          }}
+        >
+          <Grid
+            xs={10}
             sm={8}
             md={4}
             lg={4}
             xl={4}
             item
-            sx={{ backgroundColor: '#222222', height: { xs: "230px", sm: '230px', md: '230px', lg: '230px', xl: '230px' }, width: { xs: "50%", sm: '50%', md: '100%', lg: '100%', xl: '90%' }, borderRadius: 3 }}>
+            sx={{
+              backgroundColor: "#222222",
+              height: {
+                xs: "230px",
+                sm: "230px",
+                md: "230px",
+                lg: "230px",
+                xl: "230px",
+              },
+              width: {
+                xs: "50%",
+                sm: "50%",
+                md: "100%",
+                lg: "100%",
+                xl: "90%",
+              },
+              borderRadius: 3,
+            }}
+          >
             <Grid item m={3}>
-              <Typography variant="h6" sx={{ backgroundColor: "#FFDC60", borderRadius: 4, width: '100px', textAlign: "center", fontSize: '20px' }}>Contact</Typography>
+              <Typography
+                variant="h6"
+                sx={{
+                  backgroundColor: "#FFDC60",
+                  borderRadius: 4,
+                  width: "100px",
+                  textAlign: "center",
+                  fontSize: "20px",
+                }}
+              >
+                Contact
+              </Typography>
               <br />
-              <hr style={{ color: '#ECECEC' }} />
-              <Typography sx={{ color: "white", fontSize: '23px', marginTop: '8px' }}>
+              <hr style={{ color: "#ECECEC" }} />
+              <Typography
+                sx={{ color: "white", fontSize: "23px", marginTop: "8px" }}
+              >
                 <span style={{ color: "#5956E8" }}>
                   <MailOutlineIcon sx={{ fontSize: "33px" }} />
-                </span> Email
+                </span>{" "}
+                Email
               </Typography>
-              <Typography sx={{ color: '#C0BEBE', marginLeft: "40px", paddingRight: "100px", fontSize: { sm: "20px", md: "17px", lg: "18px", xl: "18px" } }}>
+              <Typography
+                sx={{
+                  color: "#C0BEBE",
+                  marginLeft: "40px",
+                  paddingRight: "100px",
+                  fontSize: { sm: "20px", md: "17px", lg: "18px", xl: "18px" },
+                }}
+              >
                 nextlevel@marketing.com
               </Typography>
             </Grid>
           </Grid>
 
-          <Grid xs={10}
+          <Grid
+            xs={10}
             sm={8}
             md={4}
             lg={4}
-            xl={4} sx={{ backgroundColor: '#222222', height: { xs: "230px", sm: '230px', md: '230px', lg: '230px', xl: '230px' }, borderRadius: 3 }}
-            item>
+            xl={4}
+            sx={{
+              backgroundColor: "#222222",
+              height: {
+                xs: "230px",
+                sm: "230px",
+                md: "230px",
+                lg: "230px",
+                xl: "230px",
+              },
+              borderRadius: 3,
+            }}
+            item
+          >
             <Grid item m={3}>
-              <Typography variant="h6" sx={{ backgroundColor: "#FFDC60", borderRadius: 4, width: '70px', textAlign: "center", fontSize: '20px' }}>Call</Typography>
+              <Typography
+                variant="h6"
+                sx={{
+                  backgroundColor: "#FFDC60",
+                  borderRadius: 4,
+                  width: "70px",
+                  textAlign: "center",
+                  fontSize: "20px",
+                }}
+              >
+                Call
+              </Typography>
               <br />
-              <hr style={{ color: '#ECECEC' }} />
-              <Typography sx={{ color: "white", fontSize: '23px', marginTop: '8px', fontWeight: "100px" }}>
+              <hr style={{ color: "#ECECEC" }} />
+              <Typography
+                sx={{
+                  color: "white",
+                  fontSize: "23px",
+                  marginTop: "8px",
+                  fontWeight: "100px",
+                }}
+              >
                 <span style={{ color: "#5956E8" }}>
                   <LocalPhoneOutlinedIcon sx={{ fontSize: "33px" }} />
-                </span> Phone
+                </span>{" "}
+                Phone
               </Typography>
-              <Typography sx={{ color: '#C0BEBE', marginLeft: "40px" }}>+91 8888884565</Typography>
+              <Typography sx={{ color: "#C0BEBE", marginLeft: "40px" }}>
+                +91 8888884565
+              </Typography>
             </Grid>
           </Grid>
         </Grid>
       </Grid>
 
-      <Grid container sx={{ marginTop: 10, marginBottom: 10, backgroundColor: "#222222", width: "100%", borderRadius: 3, display: "flex", justifyContent: "center" }}>
+      <Grid
+        container
+        sx={{
+          marginTop: 10,
+          marginBottom: 10,
+          backgroundColor: "#222222",
+          width: "100%",
+          borderRadius: 3,
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
         <form ref={form} onSubmit={sendEmail}>
-          <Grid container sx={{ margin: { xs: "30px 30px 30px", sm: "30px 60px 30px", md: "50px 60px 50px", lg: "60px 60px 50px", xl: "60px 60px 50px" }, display: "flex", justifyContent: "space-between" }}>
-            <Grid item
+          <Grid
+            container
+            sx={{
+              margin: {
+                xs: "30px 30px 30px",
+                sm: "30px 60px 30px",
+                md: "50px 60px 50px",
+                lg: "60px 60px 50px",
+                xl: "60px 60px 50px",
+              },
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <Grid
+              item
               xs={12}
               sm={12}
               md={5}
               lg={5}
-              xl={5} sx={{ marginTop: "20px" }}>
-              <label style={{ color: "white" }}>Full Name:</label><br />
+              xl={5}
+              sx={{ marginTop: "20px" }}
+            >
+              <label style={{ color: "white" }}>Full Name:</label>
+              <br />
               <TextField
                 className="bg-[#313131] font-poppins rounded-lg"
                 variant="outlined"
@@ -111,25 +242,35 @@ const Contact = () => {
                   style: { color: "white", fontFamily: "Poppins" },
                 }}
                 sx={{
-                  '& .MuiOutlinedInput-root': {
-                    '& fieldset': { borderColor: "white" },
-                    '&:hover fieldset': { borderColor: "white" },
-                    '&.Mui-focused fieldset': { borderColor: "white" },
-                    '& .MuiInputBase-input::placeholder': { color: "white", opacity: 1 },
-                    '& .MuiInputBase-input': { color: 'white' }, // Set input text color to white
+                  "& .MuiOutlinedInput-root": {
+                    "& fieldset": { borderColor: "white" },
+                    "&:hover fieldset": { borderColor: "white" },
+                    "&.Mui-focused fieldset": { borderColor: "white" },
+                    "& .MuiInputBase-input::placeholder": {
+                      color: "white",
+                      opacity: 1,
+                    },
+                    "& .MuiInputBase-input": { color: "white" }, // Set input text color to white
                   },
-                  border: "1px solid white", marginTop: 1, borderRadius: 2, width: "100%"
+                  border: "1px solid white",
+                  marginTop: 1,
+                  borderRadius: 2,
+                  width: "100%",
                 }}
               />
             </Grid>
 
-            <Grid item
+            <Grid
+              item
               xs={12}
               sm={12}
               md={5}
               lg={5}
-              xl={5} sx={{ marginTop: "20px" }}>
-              <label style={{ color: "white" }}>Your Email:</label><br />
+              xl={5}
+              sx={{ marginTop: "20px" }}
+            >
+              <label style={{ color: "white" }}>Your Email:</label>
+              <br />
               <TextField
                 variant="outlined"
                 name="mail_id" // Add name attribute
@@ -140,21 +281,40 @@ const Contact = () => {
                   style: { color: "white", fontFamily: "Poppins" },
                 }}
                 sx={{
-                  '& .MuiInputBase-input::placeholder': { color: "white", opacity: 1 },
-                  '& .MuiInputBase-input': { color: 'white' }, // Set input text color to white
-                  border: "1px solid white", marginTop: 1, borderRadius: 2, width: "100%"
+                  "& .MuiInputBase-input::placeholder": {
+                    color: "white",
+                    opacity: 1,
+                  },
+                  "& .MuiInputBase-input": { color: "white" }, // Set input text color to white
+                  border: "1px solid white",
+                  marginTop: 1,
+                  borderRadius: 2,
+                  width: "100%",
                 }}
               />
             </Grid>
           </Grid>
 
-          <Grid sx={{ color: "white", margin: { xs: "0px 30px 30px", sm: "0px 60px 30px", md: "0px 60px 30px", lg: "0px 60px 30px", xl: "0px 60px 30px" } }} item
+          <Grid
+            sx={{
+              color: "white",
+              margin: {
+                xs: "0px 30px 30px",
+                sm: "0px 60px 30px",
+                md: "0px 60px 30px",
+                lg: "0px 60px 30px",
+                xl: "0px 60px 30px",
+              },
+            }}
+            item
             xs={12}
             sm={12}
             md={12}
             lg={12}
-            xl={12}>
-            <label>Message:</label><br />
+            xl={12}
+          >
+            <label>Message:</label>
+            <br />
             <TextField
               className="bg-[#313131] font-poppins rounded-lg"
               multiline
@@ -166,28 +326,39 @@ const Contact = () => {
                 style: { color: "white", fontFamily: "Poppins" },
               }}
               sx={{
-                '& .MuiInputBase-input::placeholder': { color: "white", opacity: 1 },
-                '& .MuiInputBase-input': { color: 'white' }, // Set input text color to white
-                border: "1px solid white", marginTop: 1, borderRadius: 2, width: "100%"
+                "& .MuiInputBase-input::placeholder": {
+                  color: "white",
+                  opacity: 1,
+                },
+                "& .MuiInputBase-input": { color: "white" }, // Set input text color to white
+                border: "1px solid white",
+                marginTop: 1,
+                borderRadius: 2,
+                width: "100%",
               }}
             />
           </Grid>
 
-          <Grid item sx={{ display: "flex", justifyContent: "center", marginBottom: 6 }}>
+          <Grid
+            item
+            sx={{ display: "flex", justifyContent: "center", marginBottom: 6 }}
+          >
             <Button
               type="submit"
               className="hover:bg-[#FFDC60]"
-              variant='contained'
-              sx={{ backgroundColor: '#ffdd60ae', color: "black", height: "40px", width: "145px" }}
+              variant="contained"
+              sx={{
+                backgroundColor: "#ffdd60ae",
+                color: "black",
+                height: "40px",
+                width: "145px",
+              }}
             >
               Send Message
             </Button>
           </Grid>
         </form>
       </Grid>
-
-
-
     </Container>
   );
 };
