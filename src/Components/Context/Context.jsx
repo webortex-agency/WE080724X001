ffed9607d5438a1b8158a8fb4e61e6b35f35afff
sisindri <sisindrisingamsetti@gmail.com>
2024-08-25 21:48:49 +0530

    updated scrolling components and testimonials data from context to normal

--- src/Components/Context/Context.jsx
+++ src/Components/Context/Context.jsx
@@ -25,73 +25,6 @@ export class Provider extends Component {
       },
     ],
 
-    servicesData: [
-      {
-        id: 1,
-        image: "/Assets/Services/picture-dynamic-color.svg",
-        title: "Graphic Designing",
-        description:
-          "We have extensive graphic design experience, and we provide high-quality, more accurate outcomes.",
-        path: "/services/graphic-design",
-      },
-      {
-        id: 2,
-        image: "src/assets/Services/image 17.svg",
-        title: "Motion Graphics",
-        description:
-          "Our motion graphics bring your ideas to life with dynamic animations. ",
-        path: "/services/motion-graphics",
-      },
-      {
-        id: 3,
-        image: "src/assets/Services/paint-kit-dynamic-color.svg",
-        title: "Interior Design",
-        description:
-          "We create innovative and stylish interior spaces tailored to your needs.",
-        path: "/services/interior-design",
-      },
-      {
-        id: 4,
-        image: "src/assets/Services/webdev.svg",
-        title: "Web Design",
-        description:
-          "Our web designs are user-friendly and visually stunning, enhancing your online presence.",
-        path: "/services/web-design",
-      },
-      {
-        id: 5,
-        image: "src/assets/Services/camera-dynamic-color.svg",
-        title: "Photography",
-        description:
-          "We capture your moments with professional and creative photography.",
-        path: "/services/photography",
-      },
-      {
-        id: 6,
-        image: "src/assets/Services/digitalmarketing.png",
-        title: "Digital Marketing",
-        description:
-          "Our main goal is to provide 100% results and maintain social media with high precision.",
-        path: "/services/digital-marketing",
-      },
-      {
-        id: 7,
-        image: "src/assets/Services/image 15.svg",
-        title: "Cinematography",
-        description:
-          "We have extensive expertise editing videos, and we provide amazing insights into your thoughts.",
-        path: "/services/cinematography",
-      },
-      {
-        id: 8,
-        image: "src/assets/Services/3d-dynamic-color.svg",
-        title: "3D Cinematography",
-        description:
-          "We produce immersive 3D cinematography that captivates and engages viewers.",
-        path: "/services/3d-cinematography",
-      },
-    ],
-
     graphicProjects: [
       {
         id: 1,
@@ -172,41 +105,6 @@ export class Provider extends Component {
         id: 16,
         pic: "../src/assets/Projects/dummy.png",
         btnText: "sample image",
-      },
-    ],
-
-    testimonialsData: [
-      {
-        id: 1,
-        content:
-          "Lorem Ipsum has been the industry's standard from dummy text ever since the unknown printer to galley of type and make a type specimen book.",
-        image: "src/assets/Testimonials/Image1.png",
-        name: "John Carter",
-        role: "Marketing Lead at Google",
-      },
-      {
-        id: 2,
-        content:
-          "Lorem Ipsum has been the industry's standard from dummy text ever since the unknown printer to galley of type and make a type specimen book.",
-        image: "src/assets/Testimonials/Image2.png",
-        name: "Peeter pawl",
-        role: "Designer",
-      },
-      {
-        id: 3,
-        content:
-          "Lorem Ipsum has been the industry's standard from dummy text ever since the unknown printer to galley of type and make a type specimen book.",
-        image: "src/assets/Testimonials/Image2.png",
-        name: "Peeter pawl",
-        role: "Designer",
-      },
-      {
-        id: 4,
-        content:
-          "Lorem Ipsum has been the industry's standard from dummy text ever since the unknown printer to galley of type and make a type specimen book.",
-        image: "src/assets/Testimonials/Image2.png",
-        name: "Peeter pawl",
-        role: "Designer",
       },
     ],
 
--- src/Components/ScrollingCompo/ScrollingCompo.jsx
+++ src/Components/ScrollingCompo/ScrollingCompo.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 import { Box } from "@mui/material";
 import { styled, keyframes } from "@mui/system";
+import scrollingCompoImg1 from "../../assets/ScrollingCompo/1.svg";
+import scrollingCompoImg2 from "../../assets/ScrollingCompo/2.svg";
+import scrollingCompoImg3 from "../../assets/ScrollingCompo/3.svg";
+import scrollingCompoImg4 from "../../assets/ScrollingCompo/4.svg";
+import scrollingCompoImg5 from "../../assets/ScrollingCompo/5.svg";
+import scrollingCompoImg6 from "../../assets/ScrollingCompo/6.svg";
+import scrollingCompoImg7 from "../../assets/ScrollingCompo/7.svg";
+import scrollingCompoImg8 from "../../assets/ScrollingCompo/8.svg";
+import scrollingCompoImg9 from "../../assets/ScrollingCompo/9.svg";
+import scrollingCompoImg10 from "../../assets/ScrollingCompo/10.svg";
+import scrollingCompoImg11 from "../../assets/ScrollingCompo/11.svg";
+import scrollingCompoImg12 from "../../assets/ScrollingCompo/12.svg";
+import scrollingCompoImg13 from "../../assets/ScrollingCompo/13.svg";
+import scrollingCompoImg14 from "../../assets/ScrollingCompo/14.svg";
+import scrollingCompoImg15 from "../../assets/ScrollingCompo/15.svg";
+import scrollingCompoImg16 from "../../assets/ScrollingCompo/16.svg";
+import scrollingCompoImg17 from "../../assets/ScrollingCompo/17.svg";
 
 const scrollX = keyframes`
   0% { transform: translateX(0); }
@@ -19,77 +36,6 @@ const AnimatedMarquee = styled(Box)(({ theme }) => ({
   transition: ".3s ease",
 }));
 
-const scrollingComponents = [
-  {
-    id: 1,
-    src: "./src/assets/ScrollingCompo/1.svg",
-  },
-  {
-    id: 2,
-    src: "src/assets/ScrollingCompo/2.svg",
-  },
-  {
-    id: 3,
-    src: "src/assets/ScrollingCompo/3.svg",
-  },
-  {
-    id: 4,
-    src: "src/assets/ScrollingCompo/4.svg",
-  },
-  {
-    id: 5,
-    src: "src/assets/ScrollingCompo/5.svg",
-  },
-  {
-    id: 6,
-    src: "src/assets/ScrollingCompo/6.svg",
-  },
-  {
-    id: 7,
-    src: "src/assets/ScrollingCompo/7.svg",
-  },
-  {
-    id: 8,
-    src: "src/assets/ScrollingCompo/8.svg",
-  },
-  {
-    id: 9,
-    src: "src/assets/ScrollingCompo/9.svg",
-  },
-  {
-    id: 10,
-    src: "src/assets/ScrollingCompo/10.svg",
-  },
-  {
-    id: 11,
-    src: "src/assets/ScrollingCompo/11.svg",
-  },
-  {
-    id: 12,
-    src: "src/assets/ScrollingCompo/12.svg",
-  },
-  {
-    id: 13,
-    src: "src/assets/ScrollingCompo/13.svg",
-  },
-  {
-    id: 14,
-    src: "src/assets/ScrollingCompo/14.svg",
-  },
-  {
-    id: 15,
-    src: "src/assets/ScrollingCompo/15.svg",
-  },
-  {
-    id: 16,
-    src: "src/assets/ScrollingCompo/16.svg",
-  },
-  {
-    id: 17,
-    src: "src/assets/ScrollingCompo/17.svg",
-  },
-];
-
 const ScrollingCompo = () => {
   return (
     <Box className="relative w-full h-48 overflow-hidden flex justify-center items-center">
@@ -98,36 +44,455 @@ const ScrollingCompo = () => {
 
       <ScrollingContainer>
         <AnimatedMarquee>
-          {scrollingComponents.map((scrollingComponent) => (
-            <Box
-              key={`scrollingComponent-${scrollingComponent.id}`}
-              component="img"
-              src={scrollingComponent.src}
-              alt={`scrollingComponent ${scrollingComponent.id}`}
-              className="mx-3"
-              sx={{
-                height: "90px",
-                width: "60px",
-                objectFit: "cover",
-                objectPosition: "center",
-              }}
-            />
-          ))}
-          {scrollingComponents.map((scrollingComponent) => (
-            <Box
-              key={`scrollingComponent-duplicate-${scrollingComponent.id}`}
-              component="img"
-              src={scrollingComponent.src}
-              alt={`scrollingComponent ${scrollingComponent.id + "logo"}`}
-              className="mx-3"
-              sx={{
-                height: "90px",
-                width: "60px",
-                objectFit: "cover",
-                objectPosition: "center",
-              }}
-            />
-          ))}
+          {/* 1st Iteration */}
+
+          <Box
+            component="img"
+            src={scrollingCompoImg1}
+            alt="Component 01"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg2}
+            alt="Component 02"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg3}
+            alt="Component 03"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg4}
+            alt="Component 04"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg5}
+            alt="Component 05"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg6}
+            alt="Component 06"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg7}
+            alt="Component 07"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg8}
+            alt="Component 08"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg9}
+            alt="Component 09"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg10}
+            alt="Component 10"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg11}
+            alt="Component 11"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg12}
+            alt="Component 12"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg13}
+            alt="Component 13"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg14}
+            alt="Component 14"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg15}
+            alt="Component 15"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg16}
+            alt="Component 16"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg17}
+            alt="Component 17"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          {/* 1st Iteration */}
+
+          {/* 2nd Iteration */}
+
+          <Box
+            component="img"
+            src={scrollingCompoImg1}
+            alt="Component 01"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg2}
+            alt="Component 02"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg3}
+            alt="Component 03"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg4}
+            alt="Component 04"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg5}
+            alt="Component 05"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg6}
+            alt="Component 06"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg7}
+            alt="Component 07"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg8}
+            alt="Component 08"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg9}
+            alt="Component 09"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg10}
+            alt="Component 10"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg11}
+            alt="Component 11"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg12}
+            alt="Component 12"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg13}
+            alt="Component 13"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg14}
+            alt="Component 14"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg15}
+            alt="Component 15"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg16}
+            alt="Component 16"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+
+          <Box
+            component="img"
+            src={scrollingCompoImg17}
+            alt="Component 17"
+            className="mx-3"
+            sx={{
+              height: "90px",
+              width: "60px",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
+          
+          {/* 2nd Iteration */}
         </AnimatedMarquee>
       </ScrollingContainer>
     </Box>
