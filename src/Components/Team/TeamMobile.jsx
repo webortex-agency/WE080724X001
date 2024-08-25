f8e079cc5a5e27a5ad3ed2fc73f86399cc6e2825
sisindri <sisindrisingamsetti@gmail.com>
2024-08-25 23:25:05 +0530

    updated team in mobile view all data from context to normal

--- src/Components/Team/TeamMobile.jsx
+++ src/Components/Team/TeamMobile.jsx
@@ -7,9 +7,9 @@ import x from "../../assets/Social/twitter.png";
 import { Handshake } from "@mui/icons-material";
 import teamMember1 from "../../assets/TeamMembers/pavan.jpg";
 import teamMember2 from "../../assets/TeamMembers/vinay.jpg";
-import teamMember3 from "../../assets/TeamMembers/Prathyusha.jpg";
-import teamMember4 from "../../assets/TeamMembers/indu.jpg";
-import teamMember5 from "../../assets/TeamMembers/rishitha.jpg";
+import teamMember5 from "../../assets/TeamMembers/indu.jpg";
+import teamMember6 from "../../assets/TeamMembers/rishitha.jpg";
+import teamMember7 from "../../assets/TeamMembers/Prathyusha.jpg";
 
 const cardVariants = {
   offscreen: {
@@ -181,7 +181,395 @@ const TeamMobile = () => {
         </motion.div>
       </motion.div>
 
-      {/* Additional team members would be added here in a similar format */}
+      {/* Team Member 3 */}
+      <motion.div
+        className="card-container"
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+      >
+        <div
+          className="splash"
+          style={{
+            background:
+              "linear-gradient(306deg, hsl(60, 100%, 50%), hsl(90, 100%, 50%))",
+          }}
+        />
+        <motion.div className="card" variants={cardVariants}>
+          <div className="bg-headColor font-poppins p-6 text-center mt-1 cursor-pointer rounded-3xl hover:bg-gradient-to-tr from-[#E9F7FF] via-[#FFDBD4] to-[#FFF3CA] transition-all duration-500 ease-in-out w-full">
+            <img
+              src={teamMember7}
+              alt="team-mem-3"
+              className="rounded-t-lg"
+              style={{
+                height: "220px",
+                width: "100%",
+                objectFit: "cover",
+              }}
+            />
+            <Grid
+              sx={{
+                border: "2px solid #5956E8",
+                marginTop: 2,
+                marginLeft: "30%",
+                marginRight: "30%",
+                borderRadius: "10px",
+                backgroundColor: "#5956E8",
+              }}
+            ></Grid>
+            <Box>
+              <Typography className="pt-2 font-bold font-poppins">
+                Hitesh Mehra
+              </Typography>
+              <Typography className="text-base font-poppins text-[#555555]">
+                Creative Director
+              </Typography>
+            </Box>
+            <Box sx={{ display: "flex", justifyContent: "center", padding: 2 }}>
+              <IconButton>
+                <Link href="https://www.instagram.com/">
+                  <img src={Insta} alt="Instagram" width={25}></img>
+                </Link>
+              </IconButton>
+              <IconButton>
+                <Link href="https://www.facebook.com/">
+                  <img src={fb} alt="Facebook" width={20}></img>
+                </Link>
+              </IconButton>
+              <IconButton>
+                <Link href="https://x.com/?lang=en">
+                  <img src={x} alt="Twitter" width={20}></img>
+                </Link>
+              </IconButton>
+            </Box>
+          </div>
+        </motion.div>
+      </motion.div>
+
+      {/* Team Member 4 */}
+      <motion.div
+        className="card-container"
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+      >
+        <div
+          className="splash"
+          style={{
+            background:
+              "linear-gradient(306deg, hsl(80, 100%, 50%), hsl(120, 100%, 50%))",
+          }}
+        />
+        <motion.div className="card" variants={cardVariants}>
+          <div className="bg-headColor font-poppins p-6 text-center mt-1 cursor-pointer rounded-3xl hover:bg-gradient-to-tr from-[#E9F7FF] via-[#FFDBD4] to-[#FFF3CA] transition-all duration-500 ease-in-out w-full">
+            <img
+              src={teamMember1}
+              alt="team-mem-4"
+              className="rounded-t-lg"
+              style={{
+                height: "220px",
+                width: "100%",
+                objectFit: "cover",
+              }}
+            />
+            <Grid
+              sx={{
+                border: "2px solid #5956E8",
+                marginTop: 2,
+                marginLeft: "30%",
+                marginRight: "30%",
+                borderRadius: "10px",
+                backgroundColor: "#5956E8",
+              }}
+            ></Grid>
+            <Box>
+              <Typography className="pt-2 font-bold font-poppins">
+                Pruthvi Raj
+              </Typography>
+              <Typography className="text-base font-poppins text-[#555555]">
+                Project Manager
+              </Typography>
+            </Box>
+            <Box sx={{ display: "flex", justifyContent: "center", padding: 2 }}>
+              <IconButton>
+                <Link href="https://www.instagram.com/">
+                  <img src={Insta} alt="Instagram" width={25}></img>
+                </Link>
+              </IconButton>
+              <IconButton>
+                <Link href="https://www.facebook.com/">
+                  <img src={fb} alt="Facebook" width={20}></img>
+                </Link>
+              </IconButton>
+              <IconButton>
+                <Link href="https://x.com/?lang=en">
+                  <img src={x} alt="Twitter" width={20}></img>
+                </Link>
+              </IconButton>
+            </Box>
+          </div>
+        </motion.div>
+      </motion.div>
+
+      {/* Team Member 5 */}
+      <motion.div
+        className="card-container"
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+      >
+        <div
+          className="splash"
+          style={{
+            background:
+              "linear-gradient(306deg, hsl(320, 100%, 50%), hsl(360, 100%, 50%))",
+          }}
+        />
+        <motion.div className="card" variants={cardVariants}>
+          <div className="bg-headColor font-poppins p-6 text-center mt-1 cursor-pointer rounded-3xl hover:bg-gradient-to-tr from-[#E9F7FF] via-[#FFDBD4] to-[#FFF3CA] transition-all duration-500 ease-in-out w-full">
+            <img
+              src={teamMember5}
+              alt="team-mem-5"
+              className="rounded-t-lg"
+              style={{
+                height: "220px",
+                width: "100%",
+                objectFit: "cover",
+              }}
+            />
+            <Grid
+              sx={{
+                border: "2px solid #5956E8",
+                marginTop: 2,
+                marginLeft: "30%",
+                marginRight: "30%",
+                borderRadius: "10px",
+                backgroundColor: "#5956E8",
+              }}
+            ></Grid>
+            <Box>
+              <Typography className="pt-2 font-bold font-poppins">
+                Indu T
+              </Typography>
+              <Typography className="text-base font-poppins text-[#555555]">
+                Creative Director
+              </Typography>
+            </Box>
+            <Box sx={{ display: "flex", justifyContent: "center", padding: 2 }}>
+              <IconButton>
+                <Link href="https://www.instagram.com/">
+                  <img src={Insta} alt="Instagram" width={25}></img>
+                </Link>
+              </IconButton>
+              <IconButton>
+                <Link href="https://www.facebook.com/">
+                  <img src={fb} alt="Facebook" width={20}></img>
+                </Link>
+              </IconButton>
+              <IconButton>
+                <Link href="https://x.com/?lang=en">
+                  <img src={x} alt="Twitter" width={20}></img>
+                </Link>
+              </IconButton>
+            </Box>
+          </div>
+        </motion.div>
+      </motion.div>
+
+      {/* Team Member 6 */}
+      <motion.div
+        className="card-container"
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+      >
+        <div
+          className="splash"
+          style={{
+            background:
+              "linear-gradient(306deg, hsl(205, 100%, 50%), hsl(245, 100%, 50%))",
+          }}
+        />
+        <motion.div className="card" variants={cardVariants}>
+          <div className="bg-headColor font-poppins p-6 text-center mt-1 cursor-pointer rounded-3xl hover:bg-gradient-to-tr from-[#E9F7FF] via-[#FFDBD4] to-[#FFF3CA] transition-all duration-500 ease-in-out w-full">
+            <img
+              src={teamMember6}
+              alt="team-mem-6"
+              className="rounded-t-lg"
+              style={{
+                height: "220px",
+                width: "100%",
+                objectFit: "cover",
+              }}
+            />
+            <Grid
+              sx={{
+                border: "2px solid #5956E8",
+                marginTop: 2,
+                marginLeft: "30%",
+                marginRight: "30%",
+                borderRadius: "10px",
+                backgroundColor: "#5956E8",
+              }}
+            ></Grid>
+            <Box>
+              <Typography className="pt-2 font-bold font-poppins">
+                Rishitha
+              </Typography>
+              <Typography className="text-base font-poppins text-[#555555]">
+                Creative Director
+              </Typography>
+            </Box>
+            <Box sx={{ display: "flex", justifyContent: "center", padding: 2 }}>
+              <IconButton>
+                <Link href="https://www.instagram.com/">
+                  <img src={Insta} alt="Instagram" width={25}></img>
+                </Link>
+              </IconButton>
+              <IconButton>
+                <Link href="https://www.facebook.com/">
+                  <img src={fb} alt="Facebook" width={20}></img>
+                </Link>
+              </IconButton>
+              <IconButton>
+                <Link href="https://x.com/?lang=en">
+                  <img src={x} alt="Twitter" width={20}></img>
+                </Link>
+              </IconButton>
+            </Box>
+          </div>
+        </motion.div>
+      </motion.div>
+
+      {/* Team Member 7 */}
+      <motion.div
+        className="card-container"
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+      >
+        <div
+          className="splash"
+          style={{
+            background:
+              "linear-gradient(306deg, hsl(260, 100%, 50%), hsl(290, 100%, 50%))",
+          }}
+        />
+        <motion.div className="card" variants={cardVariants}>
+          <div className="bg-headColor font-poppins p-6 text-center mt-1 cursor-pointer rounded-3xl hover:bg-gradient-to-tr from-[#E9F7FF] via-[#FFDBD4] to-[#FFF3CA] transition-all duration-500 ease-in-out w-full">
+            <img
+              src={teamMember7}
+              alt="team-mem-7"
+              className="rounded-t-lg"
+              style={{
+                height: "220px",
+                width: "100%",
+                objectFit: "cover",
+              }}
+            />
+            <Grid
+              sx={{
+                border: "2px solid #5956E8",
+                marginTop: 2,
+                marginLeft: "30%",
+                marginRight: "30%",
+                borderRadius: "10px",
+                backgroundColor: "#5956E8",
+              }}
+            ></Grid>
+            <Box>
+              <Typography className="pt-2 font-bold font-poppins">
+                Prathyusha
+              </Typography>
+              <Typography className="text-base font-poppins text-[#555555]">
+                Creative Director
+              </Typography>
+            </Box>
+            <Box sx={{ display: "flex", justifyContent: "center", padding: 2 }}>
+              <IconButton>
+                <Link href="https://www.instagram.com/">
+                  <img src={Insta} alt="Instagram" width={25}></img>
+                </Link>
+              </IconButton>
+              <IconButton>
+                <Link href="https://www.facebook.com/">
+                  <img src={fb} alt="Facebook" width={20}></img>
+                </Link>
+              </IconButton>
+              <IconButton>
+                <Link href="https://x.com/?lang=en">
+                  <img src={x} alt="Twitter" width={20}></img>
+                </Link>
+              </IconButton>
+            </Box>
+          </div>
+        </motion.div>
+      </motion.div>
+
+      {/* Team Member 8 */}
+      <motion.div
+        className="card-container"
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+      >
+        <div
+          className="splash"
+          style={{
+            background:
+              "linear-gradient(306deg, hsl(290, 100%, 50%), hsl(320, 100%, 50%))",
+          }}
+        />
+        <motion.div className="card" variants={cardVariants}>
+          <div className="bg-headColor font-poppins p-6 text-center mt-1 cursor-pointer rounded-3xl hover:bg-gradient-to-tr from-[#E9F7FF] via-[#FFDBD4] to-[#FFF3CA] transition-all duration-500 ease-in-out w-full">
+            <img
+              src={teamMember6}
+              alt="team-mem-8"
+              className="rounded-t-lg"
+              style={{
+                height: "220px",
+                width: "100%",
+                objectFit: "cover",
+              }}
+            />
+            <Grid
+              sx={{
+                border: "2px solid #5956E8",
+                marginTop: 2,
+                marginLeft: "30%",
+                marginRight: "30%",
+                borderRadius: "10px",
+                backgroundColor: "#5956E8",
+              }}
+            ></Grid>
+            <Box>
+              <Typography className="pt-2 font-bold font-poppins">
+                Rishitha
+              </Typography>
+              <Typography className="text-base font-poppins text-[#555555]">
+                Web Designer
+              </Typography>
+            </Box>
+            <Box sx={{ display: "flex", justifyContent: "center", padding: 2 }}>
+              <IconButton>
+                <Link href="https://www.instagram.com/">
+                  <img src={Insta} alt="Instagram" width={25}></img>
+                </Link>
+              </IconButton>
+              <IconButton>
+                <Link href="https://www.facebook.com/">
+                  <img src={fb} alt="Facebook" width={20}></img>
+                </Link>
+              </IconButton>
+              <IconButton>
+                <Link href="https://x.com/?lang=en">
+                  <img src={x} alt="Twitter" width={20}></img>
+                </Link>
+              </IconButton>
+            </Box>
+          </div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
